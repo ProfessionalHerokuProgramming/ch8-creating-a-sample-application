5128f46d16d115116bf4a9308c7a8cd0e1a43a3c
Chris Kemp <ckemp@salesforce.com>
2012-07-06 15:45:43 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuPostgresRubyExample::Application.config.secret_token = 'dbe0d224fd2b967c28024457914d5f6c19f7a315a81e3808adc0f30b1d7abc8d42225d37543e05012e2221dc61390c44d3b55f62e1dc47693ef9945851944d19'
